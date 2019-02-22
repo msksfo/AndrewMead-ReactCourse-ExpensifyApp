d11f7ac259f0f1b4185f0b819809d8b39ae698d9
Tia Esguerra <msksfo@gmail.com>
2019-02-22 10:20:31 -0800

    fix: Correct typo

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@
 // provide entry and output information to webpack, so it can expose this information to another file
 
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
     const isProduction = env === 'production';
+
+    // into the constructor function, pass in the name of the file where i want my styles to be saved
     const CSSExtract = new ExtractTextPlugin('styles.css');
 
     return (module.exports = {
@@ -25,15 +27,13 @@ module.exports = env => {
                 },
                 {
                     test: /\.s?css$/,
-                    use: [CSSExtract.extract({
-                        use: ['css-loader', 'sass-loader']
-                    })],
+                    use: CSSExtract.extract({
+                        use: ['css-loader', 'sass-loader'],
+                    }),
                 },
             ],
         },
-        plugins: [
-            CSSExtract
-        ]
+        plugins: [CSSExtract],
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
